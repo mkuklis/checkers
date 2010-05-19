392ee4c71415830a67b1e24006482cffffaf7656
Michal <michal.kuklis@gmail.com>
2010-05-18 23:25:00 -0400

    add simple server

--- server.js
+++ server.js
@@ -1,62 +1,7 @@
-var http = require('http'), 
-		url = require('url'),
-		fs = require('fs'),
-		io = require('./server/socket.io'),
-		sys = require('sys'),
-		
-send404 = function(res){
-	res.writeHead(404);
-	res.write('404');
-	res.end();
-},
-		
-server = http.createServer(function(req, res){
-	// your normal server code
-	var path = url.parse(req.url).pathname;
-	switch (path){
-		case '/':
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.write('<h1>Welcome. Try the <a href="/index.html">checkers</a> example.</h1>');
-			res.end();
-			break;
-			
-		default:
-			if (/\.(js|html|swf)$/.test(path)){
-				try {
-					var swf = path.substr(-4) == '.swf';
-					res.writeHead(200, {'Content-Type': swf ? 'application/x-shockwave-flash' : ('text/' + (path.substr(-3) == '.js' ? 'javascript' : 'html'))});
-					res.write(fs.readFileSync(__dirname + path, swf ? 'binary' : 'utf8'), swf ? 'binary' : 'utf8');
-					res.end();
-				} catch(e){ 
-					send404(res); 
-				}				
-				break;
-			}
-	
-			send404(res);
-			break;
-	}
-});
+var http = require('http'),
+    port = process.env.PORT || 8001
 
-server.listen(8080);
-		
-var buffer = [], json = JSON.stringify;
-
-io.listen(server, {
-	
-	onClientConnect: function(client){
-		client.send(json({ buffer: buffer }));
-		client.broadcast(json({ announcement: client.sessionId + ' connected' }));
-	},
-	
-	onClientDisconnect: function(client){
-		client.broadcast(json({ announcement: client.sessionId + ' disconnected' }));
-	},
-	
-	onClientMessage: function(message, client){
-		var msg = { message: [client.sessionId, message] };
-    //sys.puts(msg);
-		client.broadcast(json(msg));
-	}
-	
-});
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('Hello World\n')
+}).listen(parseInt(port))
