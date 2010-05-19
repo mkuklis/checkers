f9e10815fa1efdee80be24b5bc4d0904fe9b00eb
Michal <michal.kuklis@gmail.com>
2010-05-18 23:58:00 -0400

    add simple server

--- server/socket.io.js
+++ server/socket.io.js
@@ -4,6 +4,6 @@ require('oo');
 var sys = require('sys')
 //	Listener = require('./socket.io/listener').Listener;
 
-//this.listen = function(server, options){
+this.listen = function(server, options){
 //	return new Listener(server, options);
-//};
+};
